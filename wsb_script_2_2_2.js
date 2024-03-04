47a72eeeb564beb7b2ae0f3c15d6d5ff848001ee
BiLSrv <far1803@ya.ru>
2024-03-04 22:28:46 +0300

    fd

--- wsb_script_2_2_2.js
+++ wsb_script_2_2_2.js
@@ -462,7 +462,7 @@ $('.ptime').text(json_data.time[1].toString());
 if (json_data["sensors"]) 
 {
 //try {
-	$('.canvasT').each(function(index){
+	$('canvas').each(function(index){
 		console.log(CanvGaugeArr[index]," ind ",index);
 		CanvGaugeArr[index].update({ value: parseFloat(json_data.sensors[index]) });
 		if(index>8)
