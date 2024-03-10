5d44454fb1c8e0ac558ff893ba4237f7d1b87bd5
BiLSrv <far1803@ya.ru>
2024-03-10 21:09:02 +0300

    t75

--- wsb_script_2_2_2.js
+++ wsb_script_2_2_2.js
@@ -679,14 +679,14 @@ if (json_data["sensors"])
 		
 		if($(this).attr('class')=="canvasH1")
 		{
-			console.log(CanvGaugeArrR[index]," indL ",index," class ",$(this).attr('class'));
+			console.log(CanvGaugeArrR[index]," indH ",index," class ",$(this).attr('class'));
 			CanvGaugeArrR[index].update({ value: parseFloat(json_data.sensors[index+10])});
 		}
 	});
 	$("canvas[data-type='radial-gauge']").each(function(index){
 		if($(this).attr('class')=="canvasP1")
 		{
-			console.log(CanvGaugeArrR[index]," indR ",index," class ",$(this).attr('class'));
+			console.log(CanvGaugeArrR[index]," indP ",index," class ",$(this).attr('class'));
 		Pdat = (parseFloat(json_data.sensors[index+17])*0.750062).toFixed(2);
 			CanvGaugeArrR[index].update({ value: Pdt});
 		}
