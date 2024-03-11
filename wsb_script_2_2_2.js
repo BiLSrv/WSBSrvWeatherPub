f53aa4999d235e9215be676587846952aa582e80
BiLSrv <far1803@ya.ru>
2024-03-11 22:46:04 +0300

    gf

--- wsb_script_2_2_2.js
+++ wsb_script_2_2_2.js
@@ -924,7 +924,7 @@ $(".navia").addClass("list-group-item list-group-item-action bg-light border");
 $("#esp_tx").val("wsbuser.prints(node.heap());");
 $("#esp_urx").val("");
 
-rs = setInterval(refr_rtc, 500);
+rs = setInterval(refr_rtc, 1000);
   // --------
   // Tooltips
   // --------
