27e184c0de7643d25d435b086b5e357900d9d679
BiLSrv <far1803@ya.ru>
2024-03-17 18:08:19 +0300

    bnf

--- wsb_script_2_2_2.js
+++ wsb_script_2_2_2.js
@@ -643,6 +643,28 @@ if(WSsocket.readyState!=1)
 
 }); 
 
+// Attach a submit handler to the form
+$( "#post_fw" ).submit(function( event ) {
+ 
+  // Stop form from submitting normally
+  event.preventDefault();
+ 
+  // Get some values from elements on the page:
+  var $form = $( this ),
+    term = $form.find( "input[name='s']" ).val(),
+    url = $form.attr( "action" );
+ 
+  // Send the data using post
+  var posting = $.post( url, { s: term } );
+ 
+  // Put the results in a div
+  posting.done(function( data ) {
+    var content = $( data ).find( "#content" );
+    $( "#result" ).empty().append(content);
+  });
+});
+
+
 
 function sdmc_sh() {
     mds.removeClass("collapse show");
